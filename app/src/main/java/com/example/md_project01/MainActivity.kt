67671ee99cd0209a816f7e7f02d6cbcf455fcebd
Firpo7 <40208107+Firpo7@users.noreply.github.com>
2020-01-03 18:46:00 +0100

    [bugfix] fixed the API
    separated the API logic from the update views logic

--- app/src/main/java/com/example/md_project01/MainActivity.kt
+++ app/src/main/java/com/example/md_project01/MainActivity.kt
@@ -11,26 +11,15 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.JsonHttpResponseHandler
-import cz.msebera.android.httpclient.Header
-import org.json.JSONObject
-import java.util.*
+import java.util.Calendar
 import kotlin.collections.ArrayList
 
 class MainActivity : BaseActivity() {
 
-    val Int.dp: Int
-        get() = (this / Resources.getSystem().displayMetrics.density).toInt()
-    val Int.px: Int
-        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+    //val Int.dp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    val Int.px: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
     val DAY_LAYOUTS: IntArray = intArrayOf(R.id.day0,R.id.day1,R.id.day2,R.id.day3,R.id.day4,R.id.day5,R.id.day6)
-    val WEATHERBIT_TOKEN = "9234f1e48c8345d59ec2fac5879955e3"
-    val BASE_URL = "http://api.weatherbit.io/v2.0/forecast/daily"
-
-    var lon: Double = 0.0
-    var lat: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +29,7 @@ class MainActivity : BaseActivity() {
         windowManager.defaultDisplay.getMetrics(displayMetrics)
 
         val width = displayMetrics.widthPixels
-        val height = displayMetrics.heightPixels
+        //val height = displayMetrics.heightPixels
 
         val frameWidth = width / DAY_LAYOUTS.size
 
@@ -55,30 +44,7 @@ class MainActivity : BaseActivity() {
             (currFrame.getChildAt(0) as TextView).text = getDayName( ( (currDayOfWeek + i) % 7 ) + 1 )
         }
 
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener { location: Location? ->
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            Log.d("[Running Activity]", "altitude: ${location.altitude}")
-                            Log.d("[Running Activity]", "longitude: ${location.longitude}")
-                            Log.d("[Running Activity]", "latitude: ${location.latitude}")
-                            Log.d("[Running Activity]", "time: ${location.time}")
-                            lon = location.longitude
-                            lat = location.latitude
-                        }
-                    }
-            } else {
-                showToast("Turn on location")
-            }
-        } else {
-            Log.d("PERMISSIONS NOT GRANTED", "NO NO")
-            showToast("Permissions not granted")
-            requestPermissions()
-        }
-
-        doForecast(7, lat,lon)
+        getLocationWithCallback( ::doForecast )
 
     }
 
@@ -110,47 +76,21 @@ class MainActivity : BaseActivity() {
         view.setImageResource(id)
     }
 
-
-    private fun performRequest(urlString: String) {
-
-        val client = AsyncHttpClient()
-        var forecasts = ArrayList<String>()
-
-        Log.d("LOG_RESOURCES", R.drawable.a01d.toString())
-        Log.d("LOG_RESOURCES", R.drawable.a02d.toString())
-
-
-        client.get(urlString, object: JsonHttpResponseHandler()
-        {
-            override fun onSuccess(statusCode: Int, headers: Array<Header>?, jsonObject: JSONObject?) {
-                if(jsonObject != null) {
-                    Log.d("QUERY", jsonObject.toString())
-
-                    val arr = jsonObject.getJSONArray("data")
-                    for ( i in 0 until arr.length())
-                    {
-                        val d = arr.getJSONObject(i)
-                        Log.d("post_id", d.toString())
-                        setImageView(
-                            findViewById<FrameLayout>(DAY_LAYOUTS[i]).getChildAt(1) as ImageView,
-                            d.getJSONObject("weather").getString("icon")
-                        )
-
-                    }
-
-                }
-            }
-
-            override fun onFailure(statusCode: Int, headers: Array<Header>?, e: Throwable, jsonObject: JSONObject?) {
-                Log.e("QUERY failed", "$statusCode ${e.message}")
-            }
-        })
-
+    private fun setImageViews(results: ArrayList<String>) {
+        Log.d("[setImageViews]", "STARTING CYCLING RESULTS")
+        for ( i in 0 until results.size) {
+            setImageView(
+                findViewById<FrameLayout>(DAY_LAYOUTS[i]).getChildAt(1) as ImageView,
+                results[i]
+            )
+        }
     }
 
-    private fun doForecast(days: Int, lat: Double, lon: Double) {
-        val forgeURL = "${BASE_URL}?days=${days}&lat=${lat}&lon=${lon}&key=${WEATHERBIT_TOKEN}"
-        performRequest(forgeURL)
+    private fun doForecast(location: Location?) {
+        if(location != null) {
+            val weatherService = QueryWeatherService()
+            weatherService.doForecast(location.latitude, location.longitude, ::setImageViews)
+        }
     }
 
     fun startRunPressed(@Suppress("UNUSED_PARAMETER") view: View) {
