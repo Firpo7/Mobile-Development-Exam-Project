86019fd4d02cbc967981d6059551a696dae56a96
Firpo7 <40208107+Firpo7@users.noreply.github.com>
2020-01-13 00:04:22 +0100

    [feature] load path and show on map

--- app/src/main/java/com/example/md_project01/PathService.kt
+++ app/src/main/java/com/example/md_project01/PathService.kt
@@ -6,25 +6,15 @@ import android.os.Environment
 import android.util.Log
 import java.io.File
 import java.io.PrintWriter
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.collections.ArrayList
 
 
-class PathService(private val timestamp: Long, ctx: Context) {
-    private var distanceMade: Long = 0
-    private val latitudes: ArrayList<Double> = ArrayList()
-    private val longitudes: ArrayList<Double> = ArrayList()
+class PathService(private val timestamp: Long = 0L, ctx: Context) {
+    var distanceMade: Long = 0
+    val latitudes: ArrayList<Double> = ArrayList()
+    val longitudes: ArrayList<Double> = ArrayList()
 
     private val dir = ctx.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()
-
-    private fun checkSaveDir(): Boolean {
-        val saveDir = File(dir)
-        if (!saveDir.exists()) {
-            return saveDir.mkdir()
-        }
-        return true
-    }
 
     fun addPoint(location: Location) {
         latitudes.add( location.latitude )
@@ -32,16 +22,17 @@ class PathService(private val timestamp: Long, ctx: Context) {
     }
 
     fun save(distance: Long): Boolean {
+        if (timestamp == 0L) return false
+
         distanceMade = distance
         Log.d("[PathService]", "distanceMade: $distanceMade")
         Log.d("[PathService]", "latitudes: $latitudes")
         Log.d("[PathService]", "longitudes: $longitudes")
-        Log.d("[PathService]", this.toString() )
+        Log.d("[PathService]", this.toString())
 
-        val sdf = SimpleDateFormat("dd_MM_yyyy-HH_mm", Locale.US)
-        val filename = "${sdf.format(Date(timestamp))}.json"
+        val filename = "$timestamp.json"
         Log.d("[PathService]", "$dir/$filename")
-        return if (checkSaveDir()) {
+        return if (checkDir(dir)) {
             // directory exists or already created
             val dest = File(dir, filename)
             try {
@@ -62,5 +53,25 @@ class PathService(private val timestamp: Long, ctx: Context) {
 
     override fun toString(): String {
         return "{\"distanceMade\": \"$distanceMade\",\"latitudes\":$latitudes,\"longitudes\":$longitudes}"
+    }
+
+    companion object {
+        fun getPastPathFilesList(ctx: Context): Array<File>? {
+            val dir = ctx.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()
+            if (checkDir(dir)) {
+                val dest = File(dir)
+                return dest.listFiles()
+            }
+
+            return null
+        }
+
+        private fun checkDir(dir: String): Boolean {
+            val saveDir = File(dir)
+            if (!saveDir.exists()) {
+                return saveDir.mkdir()
+            }
+            return true
+        }
     }
 }
--- app/src/main/java/com/example/md_project01/RunningActivity.kt
+++ app/src/main/java/com/example/md_project01/RunningActivity.kt
@@ -1,19 +1,30 @@
 package com.example.md_project01
 
+import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.esri.arcgisruntime.geometry.PointCollection
+import com.esri.arcgisruntime.geometry.Polyline
+import com.esri.arcgisruntime.geometry.SpatialReference
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
 import com.esri.arcgisruntime.mapping.view.LocationDisplay
 import com.esri.arcgisruntime.mapping.view.WrapAroundMode
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import kotlinx.android.synthetic.main.activity_running.*
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
@@ -23,11 +34,15 @@ import kotlin.math.sqrt
 class RunningActivity : BaseActivity() {
 
     private lateinit var lastLocation: Location
+
+    @Volatile
     private var distance = 0.0
     private lateinit var locationDisplay: LocationDisplay
 
     private var locationCallback: LocationCallback? = null
     private lateinit var locationRequest: LocationRequest
+    private lateinit var graphicsOverlay: GraphicsOverlay
+    private var lastGraphic: Graphic? = null
 
     private var pathService: PathService? = null
     //private val locationSettingsRequest: LocationSettingsRequest? = null
@@ -84,19 +99,42 @@ class RunningActivity : BaseActivity() {
         val map = ArcGISMap(Basemap.Type.DARK_GRAY_CANVAS_VECTOR, lat, lon, 18)
         mapView.map = map
 
+        graphicsOverlay = GraphicsOverlay()
+        mapView!!.graphicsOverlays.add(graphicsOverlay)
+
         mapView.wrapAroundMode = WrapAroundMode.DISABLED
         locationDisplay = mapView.locationDisplay
     }
 
-    private fun setDistanceTextView(distance: Double?) {
-        if ( distance != null )
-            findViewById<TextView>(R.id.runningactivity_textview_distance).text = distance.toInt().toString()
+    private fun addPathLayer(ps: PathService) {
+        val points = PointCollection(SpatialReference.create(GCS_WGS84))
+
+        for (i in ps.latitudes.indices) {
+            points.add(ps.longitudes[i], ps.latitudes[i])
+        }
+
+        val polyline = Polyline(points)
+        val lineSymbol = SimpleLineSymbol(
+            SimpleLineSymbol.Style.SOLID,
+            Color.argb(255, 255, 255, 255), 5.0f
+        )
+
+        val graphic = Graphic(polyline, lineSymbol)
+
+        graphicsOverlay.graphics.remove(lastGraphic)
+        graphicsOverlay.graphics.add(graphic)
+        lastGraphic = graphic
+    }
+
+    private fun setTextView(textView: TextView, text: String?) {
+        if ( text != null )
+            textView.text = distance.toInt().toString()
     }
 
     private fun updateDistance(location: Location?) {
         if (location != null) {
             pathService?.addPoint(location)
-            if(::lastLocation.isInitialized) {
+            if (::lastLocation.isInitialized) {
                 distance += getStepDistance(lastLocation, location)
                 lastLocation = location
             } else
@@ -105,12 +143,12 @@ class RunningActivity : BaseActivity() {
             Log.d("RUNNING SCHEDULED TASK", "null Location!!")
         }
         Log.d("RUNNING SCHEDULED TASK", distance.toString())
-        setDistanceTextView(distance)
+        setTextView(findViewById(R.id.runningactivity_textview_distance), distance.toString())
     }
 
-    fun startRunning(@Suppress("UNUSED_PARAMETER")v: View) {
+    fun startRunning(@Suppress("UNUSED_PARAMETER") v: View) {
         if (!locationDisplay.isStarted) {
-            pathService = PathService(System.currentTimeMillis(), this.applicationContext)
+            pathService = PathService(System.currentTimeMillis(), this@RunningActivity)
             locationDisplay.autoPanMode = LocationDisplay.AutoPanMode.RECENTER
             locationDisplay.startAsync()
             fusedLocationClient.requestLocationUpdates(
@@ -122,7 +160,7 @@ class RunningActivity : BaseActivity() {
     }
 
     private fun savePathMade() {
-        if(checkPermissions(REQUEST_PERMISSION_READWRITE_ID)) {
+        if (checkPermissions(REQUEST_PERMISSION_READWRITE_ID)) {
             if (pathService != null) {
                 if (pathService!!.save(distance.toLong()).not())
                     showToast("ERROR WHILE WRITING THE FILE")
@@ -132,7 +170,7 @@ class RunningActivity : BaseActivity() {
     }
 
     fun stopRunning(@Suppress("UNUSED_PARAMETER") view: View) {
-        if ( locationDisplay.isStarted ) {
+        if (locationDisplay.isStarted) {
             shutdownScheduledTask()
             savePathMade()
             locationDisplay.stop()
@@ -140,7 +178,7 @@ class RunningActivity : BaseActivity() {
     }
 
     //DEBUG FUNCTION
-    fun checkGetLocation(@Suppress("UNUSED_PARAMETER")view: View) {
+    fun checkGetLocation(@Suppress("UNUSED_PARAMETER") view: View) {
         if (checkPermissions(REQUEST_PERMISSION_LOCATION_ID)) {
             if (isLocationEnabled()) {
                 fusedLocationClient.lastLocation
@@ -160,9 +198,69 @@ class RunningActivity : BaseActivity() {
         }
     }
 
+    private fun loadPathFromJSON(json: String): PathService? {
+        val ps = PathService(ctx = this@RunningActivity)
+
+        val jsonObj = JSONObject(json.substring(json.indexOf("{"), json.lastIndexOf("}") + 1))
+        val latJson = jsonObj.getJSONArray("latitudes")
+        val lonJson = jsonObj.getJSONArray("longitudes")
+        for (i in 0 until latJson.length()) {
+            try {
+                ps.latitudes.add(latJson[i].toString().toDouble())
+                ps.longitudes.add(lonJson[i].toString().toDouble())
+            } catch (e: java.lang.Exception) {
+                return null
+            }
+        }
+        ps.distanceMade = jsonObj.getString("distanceMade").toLong()
+
+        return ps
+    }
+
+    fun showPathHistories(@Suppress("UNUSED_PARAMETER") v: View) {
+        val builder = AlertDialog.Builder(this@RunningActivity)
+        builder.setTitle("Choose a path")
+
+        val fileList = PathService.getPastPathFilesList(this@RunningActivity)?.filter { f ->
+            try {
+                f.name.split(".")[0].toLong()
+
+                /*
+                 * TODO: check that files contain a valid JSON (?)
+                 */
+
+                true
+            } catch (e: NumberFormatException) {
+                false
+            }
+
+        }
+
+        if (fileList != null && fileList.isNotEmpty()) {
+            val fileNames = Array(fileList.size) { i ->
+                val timestamp = fileList[i].name.split(".")[0].toLong()
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US)
+                sdf.format(Date(timestamp))
+            }
+
+            builder.setItems(fileNames) { _, which ->
+                val ps = loadPathFromJSON(fileList[which].readText())
+                if (ps != null) {
+                    addPathLayer(ps)
+                }
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            showToast("No previous paths to show")
+        }
+    }
+
     companion object {
         private const val UPDATE_INTERVAL_MS: Long = 1000
         private const val FASTEST_UPDATE_INTERVAL_MS: Long = 1000
+        private const val GCS_WGS84 = 4326 // Geographic coordinate systems return from a GPS device
 
         private fun getStepDistance(lat1: Double, lng1: Double, lat2: Double, lng2: Double): Double {
             val earthRadius = 6371000.0 //meters
